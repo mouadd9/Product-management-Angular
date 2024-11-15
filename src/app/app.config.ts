d8af5dcca56eedc31aabf8b9b7235f206656c976
mouadispunk <bencaidmouad9@gmail.com>
2024-11-15 01:21:34 +0100

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,32 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+/*
+The root bootstrap configuration in main.ts tells Angular 
+how to start the application and provides any global settings it needs,
+such as routing, state management, or other services.
+It’s the equivalent of setting up AppModule 
+in the traditional Angular approach, but with standalone components,
+this setup is done directly in main.ts using bootstrapApplication.
+
+*/
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient() ,provideRouter(routes) ],
-  // here we provide routes , httpClient
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    provideStore(), // here we should specify the reducers
+    provideEffects([]), // here we should add an array of effects
+    provideStoreDevtools({})
+  ],
+  
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser'; // Imports the `bootstrapApplication` function, which is used to initialize (or "bootstrap") the Angular application.
 
+import { appConfig } from './app/app.config';// Imports `appConfig`, which contains the application’s configuration settings (like routes, providers, etc.).
 
-enableProdMode(); 
+import { AppComponent } from './app/app.component';// Imports the root component (`AppComponent`). This is the starting component of the application, which contains the component tree.
+
+import { enableProdMode } from '@angular/core';// Imports `enableProdMode`, a function that enables production mode for the Angular app, which disables debugging information to improve performance.
+
+enableProdMode(); // Activates production mode to enhance performance by disabling Angular’s development mode checks.
 bootstrapApplication(AppComponent, appConfig)
+// Bootstraps the application with `AppComponent` as the root component and `appConfig` as the configuration for the app.
+// `AppComponent` serves as the entry point, while `appConfig` defines global settings (e.g., routing, services).
   .catch((err) => console.error(err));
-
-  
+// Catches and logs any errors that might occur during the application bootstrapping process.
