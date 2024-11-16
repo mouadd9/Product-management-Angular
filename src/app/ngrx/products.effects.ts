5300326628d157da5bf384380913fcf943d7da2e
mouadispunk <bencaidmouad9@gmail.com>
2024-11-16 05:53:24 +0100

    added the effect mechanics

--- src/app/ngrx/products.effects.ts
+++ src/app/ngrx/products.effects.ts
@@ -1,1 +1,40 @@
 // here we will define effects that will react to special actions that require API calls
+// this will be a class , an injectable , we will inject into it an Actions observable that will help us access the actions stream 
+// in order to react to certain actions 
+// and we will inject the service that returns Observables that listens to the responses from the backend 
+
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { ProductsService } from "../services/products.service";
+import { catchError, map, mergeMap, Observable, of } from "rxjs";
+import { GetAllProductsError, GetAllProductsSuccess, ProductsActionTypes } from "./products.actions";
+import { Action } from "@ngrx/store";
+
+@Injectable()
+export class ProductsEffects {
+    // we use private to automatically declare and initialize actions$ and to make it accessible within the class
+    constructor(private actions$ : Actions , private productsService : ProductsService){}
+
+    // next step is to create an Effect that : 
+    // listens to specific actions
+    // callsthe products service to fetch data 
+    // dispatches an action based on the response 
+
+    // by convention an effect has the same name as the action it responds to 
+    // and an effect is an Observable of type Action , meaning the dispatched action (success or failure)
+
+    getAllProductsEffect$ : Observable<Action> = createEffect(() => 
+
+        this.actions$.pipe( // we use pipe to go through the events (actions) that are emitted to the stream
+            ofType(ProductsActionTypes.GET_ALL_PRODUCTS), // when we find an event or action of this type
+            mergeMap( () => {
+               return this.productsService.getAllProducts().pipe(
+                        map(products => new GetAllProductsSuccess(products) ),
+                        catchError((err) => of(new GetAllProductsError(err.message)) )
+                )
+            })
+            
+        )
+        
+    );
+}
--- src/app/ngrx/products.reducers.ts
+++ src/app/ngrx/products.reducers.ts
@@ -1,1 +1,49 @@
 // here we will define reducers that will return a new state to the store and react to actions
+
+import { Product } from '../models/product.model';
+import { ProductsActions, ProductsActionTypes } from './products.actions';
+
+// here we will modify the state
+// here we define what the state will look like
+// and here we will declare the diff forms a state can take
+
+export enum ProductsStateEnum {
+  // either the state is Idle/Loading/Loaded or Encountered an error while loading
+  LOADING = 'Loading',
+  LOADED = 'Loaded',
+  ERROR = 'Error',
+  INITIAL = 'Initial',
+}
+
+// now that we defined the different phases that our state can go through
+// we should define the structure of our state
+
+export interface ProductsState {
+  products: Product[]; // this is the actual data
+  dataState: ProductsStateEnum; // this is its state
+  errorMessage: string; // this is the error message if the state is in ERROR
+}
+
+const initialState: ProductsState = {
+  products: [],
+  dataState: ProductsStateEnum.INITIAL,
+  errorMessage: '',
+};
+
+// we should a function , that takes as input , the current state , and then the ACTION dispatched
+
+export function productsReducer( action: ProductsActions, state: ProductsState = initialState ): ProductsState {
+  switch (action.type) {
+    case ProductsActionTypes.GET_ALL_PRODUCTS:
+        return {... state , dataState : ProductsStateEnum.LOADING} ; 
+      break;
+    case ProductsActionTypes.GET_ALL_PRODUCTS_ERROR:
+        return {...state, dataState : ProductsStateEnum.ERROR, errorMessage : action.payload }
+      break;
+    case ProductsActionTypes.GET_ALL_PRODUCTS_SUCCESS:
+        return {...state, dataState : ProductsStateEnum.LOADED, products : action.payload }
+      break;
+    default: return {...state} ; 
+      break;
+  }
+};  
